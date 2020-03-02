ae4aaff9459e2f09a4347f2cccd6c9d5f2f8d02a
derek@derekbelrose.com <dbelrose@jhu.edu>
2020-03-02 10:47:11 -0500

    Add env var for CERT_YEARS
    Allows setting lifetime of a cert in years. Default is 10.
    Updated README
    Fixed newlines at bottom of entrypiont and readme.

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -2,6 +2,9 @@
 
 # ENV VARS:
 #   - $FQDN
+#   - $CERT_YEARS
 
-/keygen.sh -h $FQDN -y 10 -e https://$FQDN/shibboleth -f -o /working
+YEARS=${CERT_YEARS:-10}
+
+/keygen.sh -h $FQDN -y $YEARS -e https://$FQDN/shibboleth -f -o /working
 /metagen.sh -c sp-cert.pem -h $FQDN -e https://$FQDN/shibboleth > /working/$FQDN.xml
