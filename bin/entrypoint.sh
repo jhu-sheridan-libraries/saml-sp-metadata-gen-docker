bee00f00ea1cabc1d70f67829b2dd87773dbf8c6
Derek Belrose <derekbelrose@users.noreply.github.com>
2020-03-02 10:04:37 -0500

    metagen was not outputing to /working. That's fixed. (#2)

--- bin/entrypoint.sh
+++ bin/entrypoint.sh
@@ -4,4 +4,4 @@
 #   - $FQDN
 
 /keygen.sh -h $FQDN -y 10 -e https://$FQDN/shibboleth -f -o /working
-/metagen.sh -c sp-cert.pem -h $FQDN -e https://$FQDN/shibboleth > $FQDN.xml
+/metagen.sh -c sp-cert.pem -h $FQDN -e https://$FQDN/shibboleth > /working/$FQDN.xml
